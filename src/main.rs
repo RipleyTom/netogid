0abf9be30a2d1107e41900adac857949b954af23
RipleyTom <RipleyTom@users.noreply.github.com>
2024-10-06 19:49:54 +0200

    version 0.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use core::str;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 use std::io::Read;
@@ -19,14 +22,33 @@ enum ErrorType {
 	MissingData,
 }
 
-fn process_file(file: &Path) -> Result<String, ErrorType> {
-	if file.file_name().unwrap().to_str().unwrap() != "PARAM.SFO" {
+struct ParsingResult {
+	comm_id: String,
+	title_id: String,
+	title: String,
+}
+
+fn process_tropconf(path: &Path) -> Result<String, ErrorType> {
+	let path_conf = path.with_file_name("TROPCONF.SFM");
+
+	let mut file = fs::File::open(path_conf).map_err(ErrorType::FileIO)?;
+	let mut data = Vec::new();
+	file.read_to_end(&mut data).map_err(ErrorType::FileIO)?;
+
+	let str = str::from_utf8(&data[0x40..]).unwrap();
+	let (_, data) = str.split_once("<title-name>").ok_or_else(|| ErrorType::MissingData)?;
+	let (name, _) = data.split_once("</title-name>").ok_or_else(|| ErrorType::MissingData)?;
+
+	Ok(String::from(name))
+}
+
+fn process_file(path: &Path) -> Result<ParsingResult, ErrorType> {
+	if path.file_name().unwrap().to_str().unwrap() != "PARAM.SFO" {
 		return Err(ErrorType::FileNotParam);
 	}
 
-	let mut file = fs::File::open(file).map_err(ErrorType::FileIO)?;
+	let mut file = fs::File::open(path).map_err(ErrorType::FileIO)?;
 	let mut data = Vec::new();
-
 	file.read_to_end(&mut data).map_err(ErrorType::FileIO)?;
 
 	let (_, parsed_data) = parser::parse_param_sfo(&data).map_err(|_| ErrorType::ParsingError)?;
@@ -36,30 +58,57 @@ fn process_file(file: &Path) -> Result<String, ErrorType> {
 	}
 
 	let str_commid = parsed_data["NPCOMMID"].to_string();
-	let parsed_comm_id = if let Some((id, _)) = str_commid.split_once('_') {
+	let comm_id = if let Some((id, _)) = str_commid.split_once('_') {
 		id.to_owned()
 	} else {
 		return Err(ErrorType::MissingData);
 	};
 
-	Ok(format!("{}=>{}", parsed_comm_id, parsed_data["TITLEID000"]))
+	let title_id = parsed_data["TITLEID000"].to_string();
+
+	let title = match process_tropconf(path) {
+		Ok(v) => v,
+		_ => String::new(),
+	};
+
+	Ok(ParsingResult { comm_id, title_id, title })
 }
 
-fn process_directory(dir: &Path) -> anyhow::Result<()> {
+struct CommIdInfo {
+	titles: HashSet<String>,
+	title_ids: HashSet<String>,
+}
+
+impl CommIdInfo {
+	fn default() -> CommIdInfo {
+		CommIdInfo {
+			titles: HashSet::new(),
+			title_ids: HashSet::new(),
+		}
+	}
+}
+
+fn process_directory(dir: &Path, results: &mut HashMap<String, CommIdInfo>) -> anyhow::Result<()> {
 	let dir = fs::read_dir(dir)?;
 
 	for d in dir {
 		let d = d?;
 		let path = d.path();
 		if path.is_dir() {
-			process_directory(&path)?;
+			process_directory(&path, results)?;
 		} else {
 			let res = process_file(&path);
 			if res.is_err() {
 				continue;
 			}
 			let res = res.unwrap();
-			println!("{}", res);
+
+			let entry = results.entry(res.comm_id).or_insert(CommIdInfo::default());
+			entry.title_ids.insert(res.title_id);
+
+			if !res.title.is_empty() {
+				entry.titles.insert(res.title);
+			}
 		}
 	}
 
@@ -76,7 +125,27 @@ fn main() {
 		return;
 	}
 
-	if let Err(e) = process_directory(Path::new(&args[1])) {
+	let mut results: HashMap<String, CommIdInfo> = HashMap::new();
+
+	if let Err(e) = process_directory(Path::new(&args[1]), &mut results) {
 		println!("Failed: {}", e);
 	}
+
+	let mut json_data = json::JsonValue::new_object();
+
+	for (com_id, data) in &results {
+		let mut array = json::JsonValue::new_array();
+		for title_id in &data.title_ids {
+			array.push(json::JsonValue::String(title_id.clone())).unwrap();
+		}
+		json_data[com_id]["title_ids"] = array;
+
+		let mut array = json::JsonValue::new_array();
+		for title in &data.titles {
+			array.push(title.clone()).unwrap();
+		}
+		json_data[com_id]["titles"] = array;
+	}
+
+	println!("{}", json::stringify_pretty(json_data, 8));
 }
